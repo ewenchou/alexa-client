48f13ca9ecbdd21fcf88a55afe484a71ef03fd23
Andrea Pascal <andrea@anodium.net>
2017-02-28 04:49:49 -0400

    Parameterize everything

--- alexa_client/settings.py
+++ alexa_client/settings.py
@@ -1,7 +1,7 @@
-# Alexa Voice Service
-DEVICE_TYPE_ID = ''
-CLIENT_ID = ''
-CLIENT_SECRET = ''
-REFRESH_TOKEN = ''
-# Temporary directory for storing mp3 and text files
-TEMP_DIR = '/tmp/alexa-client'
+import os
+import uuid
+
+DEVICE_TYPE_ID = uuid.getnode()
+CLIENT_ID = os.environ['ALEXA_CLIENT_ID']
+CLIENT_SECRET = os.environ['ALEXA_CLIENT_SECRET']
+REFRESH_TOKEN = os.environ['ALEXA_REFRESH_TOKEN']
