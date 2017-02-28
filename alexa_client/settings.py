5294a986ade65793c91dc8d321aee0785548c8ef
Andrea Pascal <andrea@anodium.net>
2017-02-28 04:56:43 -0400

    Parameterize OAuth2 script

--- alexa_client/settings.py
+++ alexa_client/settings.py
@@ -1,7 +1,7 @@
 import os
-import uuid
 
-DEVICE_TYPE_ID = uuid.getnode()
 CLIENT_ID = os.environ['ALEXA_CLIENT_ID']
 CLIENT_SECRET = os.environ['ALEXA_CLIENT_SECRET']
+PRODUCT_ID = os.environ['ALEXA_PRODUCT_ID']
 REFRESH_TOKEN = os.environ['ALEXA_REFRESH_TOKEN']
+WEB_PORT = int(os.environ['ALEXA_PORT']) or 3000
